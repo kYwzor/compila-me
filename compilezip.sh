a5c6c1594d46b8604ffc608ac626daf7a811dc18
kYwzor <tiago_gomes_76@hotmail.com>
2018-04-04 05:12:12 +0100

    new files added to the zipper

--- compilezip.sh
+++ compilezip.sh
@@ -2,4 +2,4 @@
 lex uccompiler.l
 yacc -d -v uccompiler.y
 clang-3.8 -Wall -o uccompiler -Wno-unused-function *.c
-zip uccompiler.zip uccompiler.l uccompiler.y
+zip uccompiler.zip uccompiler.l uccompiler.y tree.c tree.h
