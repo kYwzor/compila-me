eb614fb46393dc16700c3e82ae41e74b4a059c47
soren5 <pedrofilipecarvalho@mac.com>
2018-04-04 15:47:53 +0100

    uns testes novos

--- casos_teste/teste1.3.c
+++ casos_teste/teste1.3.c
@@ -1,31 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-
-#include "Maxfiles.h"
-#include "MaxSLiCInterface.h"
-
-int main(void)
-{
-  const int inSize = 96;
-
-  int *const in = malloc(sizeof(int) * inSize);
-  assert(in != NULL);
-
-  int *const out = malloc(sizeof(int) * inSize);
-  assert(out != NULL);
-
-  for(int i=0; i < inSize; ++i)
-      in[i] = i;
-
-  printf("Running DFE...");
-  fflush(stdout);
-  Test(inSize, in, out);
-  printf("done.\n");
-
-  for(int i=0; i < inSize; ++i)
-      printf("out[%d] = %d\n", i, out[i]);
-
-  return EXIT_SUCCESS;
+int main(void){
+     if(1 == 1){
+         int ae;
+     }
+     if (x > y & z > y || z != x) { 
+         while (){{ 
+             return 1; 
+         } 
+     } 
+     else { 
+         return 0; 
+     } 
 }
