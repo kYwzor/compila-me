fd6473391fbb99984030fe5d42302acd0615cb60
kYwzor <tiago_gomes_76@hotmail.com>
2018-05-01 01:51:49 +0100

    Ridiculously good refactor

--- semantics.c
+++ semantics.c
@@ -3,8 +3,8 @@
 void print_tables()
 {
   printf("===== Global Symbol Table =====\n");
-  Table_list aux = list;
-  Sym_list global_aux = global_table->next;
+  Table_list aux = global_table;
+  Sym_list global_aux = global_table->table_node->next;
   while (global_aux != NULL)
   {
     char *s = get_string_for_tables(global_aux->label);
@@ -29,7 +29,7 @@ void print_tables()
   }
   aux = aux->next;
   printf("\n");
-  aux = list;
+  aux = global_table;
   aux = aux->next;
   while (aux != NULL)
   {
@@ -65,416 +65,179 @@ int handle_node(Node node)
 
   switch (node->label)
   {
-  case Program:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(Program));
-    list = (Table_list)malloc(sizeof(_table_list));
-    Sym_list new_node = (Sym_list)malloc(sizeof(_Sym_list));
-    new_node->name = "Global";
-    new_node->label = -1;
-    new_node->next = NULL;
-    list->table_node = new_node;
-    list->next = NULL;
+    case Program:
+    {
+      if (DEBUG)
+        printf("%s is %s\n", get_label_string(node->label), get_label_string(Program));
+      global_table = (Table_list)malloc(sizeof(_table_list));
+      Sym_list new_node = (Sym_list)malloc(sizeof(_Sym_list));
+      new_node->name = "Global";
+      new_node->label = -1;
+      new_node->next = NULL;
+      global_table->table_node = new_node;
+      global_table->next = NULL;
 
-    current_table = new_node;
-    global_table = new_node;
+      current_table = global_table;
 
-    insert_symbol(new_node, "putchar", Int);
-    Sym_list put_char_table = (Sym_list)malloc(sizeof(_Sym_list));
-    put_char_table->label = Int;
-    put_char_table->name = "putchar";
-    put_char_table->next = NULL;
-    current_table = put_char_table;
-    Node type_spec = (Node)malloc(sizeof(Node_t));
-    type_spec->label = Int;
-    Node id = (Node)malloc(sizeof(Node_t));
-    id->value = "a";
-    add_table(put_char_table, 0);
-    add_parameter(type_spec, id);
+      Node aux = (Node) malloc(sizeof(Node_t)); //paramlist
+      aux->child = (Node) malloc(sizeof(Node_t)); //ParamDeclaration
+      aux->child->brother = NULL;
+      Node type_spec = (Node)malloc(sizeof(Node_t));
+      type_spec->label = Int;
+      Node id = (Node)malloc(sizeof(Node_t));
+      id->value = "a";
+      aux->child->child = type_spec;
+      type_spec->brother = id;
+      create_function_entry("putchar", Int, aux);
 
-    insert_symbol(new_node, "getchar", Int);
-    Sym_list get_char_table = (Sym_list)malloc(sizeof(_Sym_list));
-    get_char_table->label = Int;
-    get_char_table->name = "getchar";
-    get_char_table->next = NULL;
-    current_table = get_char_table;
-    type_spec->label = Void;
-    id->value = NULL;
-    add_table(get_char_table, 0);
-    add_parameter(type_spec, id);
+      type_spec->label = Void;
+      id->value = NULL;
+      create_function_entry("getchar", Int, aux);
 
-    current_table = global_table;
+      free(id);
+      free(type_spec);
+      free(aux->child);
+      free(aux);
+      /*
+      insert_symbol(new_node, "putchar", Int);
+      Sym_list put_char_table = (Sym_list)malloc(sizeof(_Sym_list));
+      put_char_table->label = Int;
+      put_char_table->name = "putchar";
+      put_char_table->next = NULL;
+      current_table = put_char_table;
+      Node type_spec = (Node)malloc(sizeof(Node_t));
+      type_spec->label = Int;
+      Node id = (Node)malloc(sizeof(Node_t));
+      id->value = "a";
+      add_table(put_char_table, 0);
+      add_parameter(type_spec, id);
 
-    free(id);
-    free(type_spec);
-    full_expand(node);
-    break;
-  }
+      insert_symbol(new_node, "getchar", Int);
+      Sym_list get_char_table = (Sym_list)malloc(sizeof(_Sym_list));
+      get_char_table->label = Int;
+      get_char_table->name = "getchar";
+      get_char_table->next = NULL;
+      current_table = get_char_table;
+      type_spec->label = Void;
+      id->value = NULL;
+      add_table(get_char_table, 0);
+      add_parameter(type_spec, id);
 
-  case FuncDefinition:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(FuncDefinition));
-    Node type_spec;
-    Node id;
-    Node paramList;
+      current_table = global_table;
 
-    type_spec = node->child;
+      free(id);
+      free(type_spec);
+      */
+      full_expand(node);
+      break;
+    }
 
-    id = type_spec->brother;
+    case FuncDefinition:
+    {
+      if (DEBUG)
+        printf("%s is %s\n", get_label_string(node->label), get_label_string(FuncDefinition));
+      Node type_spec = node->child;
+      Node id = type_spec->brother;
+      Node paramList = id->brother;
 
-    paramList = id->brother;
+      current_table = find_function_entry(id->value);
+      // TODO: We'll probably need to check if we aren't re-defining
+      if (current_table == NULL)
+        current_table = create_function_entry(id->value, type_spec->label, paramList);  
 
-    current_table = find_function_entry(list, type_spec->label, id->value);
+      handle_node(paramList->brother); //FuncBody
+      current_table->is_defined = 1;
 
-    insert_symbol(global_table, id->value, type_spec->label);
+      current_table = global_table;
 
-    if (node->child != NULL)
-      handle_node(node->child);
+      if (node->brother != NULL)
+        handle_node(node->brother);
 
-    current_table = global_table;
-    if (node->brother != NULL)
-      handle_node(node->brother);
-    break;
-  }
+      break;
+    }
 
-  case FuncDeclaration:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(FuncDeclaration));
-    Node type_spec;
-    Node id;
-    Node paramList;
+    case FuncDeclaration:
+    {
+      if (DEBUG)
+        printf("%s is %s\n", get_label_string(node->label), get_label_string(FuncDeclaration));
+      Node type_spec = node->child;
+      Node id = type_spec->brother;
+      Node paramList = id->brother;
 
-    //Check if all the children exist
-      type_spec = node->child;
+      // TODO: We'll need to do something about re-declaring (else)
+      if (find_function_entry(id->value) == NULL)
+        create_function_entry(id->value, type_spec->label, paramList); 
 
-      id = type_spec->brother;
+      if (node->brother != NULL)
+        handle_node(node->brother);
 
-      paramList = id->brother;
+      break;
+    }
 
-    current_table = (Sym_list)malloc(sizeof(_Sym_list));
+    case Declaration:
+    {
+      if (DEBUG)
+        printf("%s is %s\n", get_label_string(node->label), get_label_string(Declaration));
+      Node type_spec = node->child;
+      Node id = type_spec->brother;
+      insert_symbol(current_table->table_node, id->value, type_spec->label);
+      full_expand(node);
+      break;
+    }
 
-    //Check if children are adequate
-      current_table->label = type_spec->label;
+    case FuncBody:
+    {
+      if (DEBUG)
+        printf("%s is %s\n", get_label_string(node->label), get_label_string(FuncBody));
+      full_expand(node);
+      break;
+    }
 
-      current_table->name = id->value;
+    /* Statements */
+    case StatList:
+    {
+      if (DEBUG)
+        printf("%s is %s\n", get_label_string(node->label), get_label_string(StatList));
+      full_expand(node);
+      break;
+    }
 
-    add_table(current_table, 0);
-    insert_symbol(global_table, id->value, type_spec->label);
+    case If:
+    {
+      if (DEBUG)
+        printf("%s is %s\n", get_label_string(node->label), get_label_string(If));
+      full_expand(node);
+      break;
+    }
 
-    if (node->child != NULL)
-      handle_node(node->child);
+    case While:
+    {
+      if (DEBUG)
+        printf("%s is %s\n", get_label_string(node->label), get_label_string(While));
+      full_expand(node);
+      break;
+    }
+    case Return:
+    {
+      if (DEBUG)
+        printf("%s is %s\n", get_label_string(node->label), get_label_string(Return));
+      full_expand(node);
+      break;
+    }
 
-    current_table = global_table;
-
-    if (node->brother != NULL)
-      handle_node(node->brother);
-
-    break;
-  }
-
-  case ParamList:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(ParamList));
-    full_expand(node);
-    break;
-  }
-
-  case ParamDeclaration:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(ParamDeclaration));
-    Node type_spec;
-    Node id;
-
-    //Check if param has type
-      type_spec = node->child;
-
-    if (type_spec->brother != NULL)
-      id = type_spec->brother;
-    else
-      id = NULL;
-
-    add_parameter(type_spec, id);
-
-    full_expand(node);
-    break;
-  }
-
-  case Declaration:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(Declaration));
-    Node type_spec = node->child;
-    Node id = type_spec->brother;
-    insert_symbol(current_table, id->value, type_spec->label);
-    full_expand(node);
-    break;
-  }
-
-  case FuncBody:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(FuncBody));
-    full_expand(node);
-    break;
-  }
-
-  case StatList:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(StatList));
-    full_expand(node);
-    break;
-  }
-
-  case If:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(If));
-    full_expand(node);
-    break;
-  }
-
-  case While:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(While));
-    full_expand(node);
-    break;
-  }
-  case Return:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(Return));
-    full_expand(node);
-    break;
-  }
-  case Or:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(Or));
-    full_expand(node);
-    break;
-  }
-  case And:
-  {
-    if (DEBUG)
-      printf("%s is %s\n", get_label_string(node->label), get_label_string(And));
-    full_expand(node);
-    break;
-  }
-  case Eq:
-  {
-    full_expand(node);
-    break;
-  }
-  case Ne:
-  {
-    full_expand(node);
-    break;
-  }
-  case Lt:
-  {
-    full_expand(node);
-    break;
-  }
-  case Gt:
-  {
-    full_expand(node);
-    break;
-  }
-  case Le:
-  {
-    full_expand(node);
-    break;
-  }
-  case Ge:
-  {
-    full_expand(node);
-    break;
-  }
-  case Add:
-  {
-    full_expand(node);
-    break;
-  }
-  case Sub:
-  {
-    full_expand(node);
-    break;
-  }
-  case Mul:
-  {
-    full_expand(node);
-    break;
-  }
-  case Div:
-  {
-    full_expand(node);
-    break;
-  }
-  case Mod:
-  {
-    full_expand(node);
-    break;
-  }
-  case Not:
-  {
-    full_expand(node);
-    break;
-  }
-  case Minus:
-  {
-    full_expand(node);
-    break;
-  }
-  case Plus:
-  {
-    full_expand(node);
-    break;
-  }
-  case Store:
-  {
-    full_expand(node);
-    break;
-  }
-  case Comma:
-  {
-    full_expand(node);
-    break;
-  }
-  case Call:
-  {
-    full_expand(node);
-    break;
-  }
-  case BitWiseAnd:
-  {
-    full_expand(node);
-    break;
-  }
-  case BitWiseXor:
-  {
-    full_expand(node);
-    break;
-  }
-  case BitWiseOr:
-  {
-    full_expand(node);
-    break;
-  }
-  case Char:
-  {
-    full_expand(node);
-    break;
-  }
-  case ChrLit:
-  {
-    full_expand(node);
-    break;
-  }
-  case Id:
-  {
-    full_expand(node);
-    break;
-  }
-  case Int:
-  {
-    full_expand(node);
-    break;
-  }
-  case Short:
-  {
-    full_expand(node);
-    break;
-  }
-  case IntLit:
-  {
-    full_expand(node);
-    break;
-  }
-  case Double:
-  {
-    full_expand(node);
-    break;
-  }
-  case RealLit:
-  {
-    full_expand(node);
-    break;
-  }
-  case Void:
-  {
-    full_expand(node);
-    break;
-  }
-  case Null:
-  {
-    full_expand(node);
-    break;
-  }
+    /* All operators, terminals and Null are defaulted for now */
+    /* ParamList and ParamDeclaration are also defaulted, but they should never occur*/
+    default:
+      printf("Defaulted %s\n", get_label_string(node->label));
+      full_expand(node);
+      break;
   }
   return 1;
 }
 
-void add_table(Sym_list table, int is_defined)
-{
-  //Verificar se ja esta na tabela
-  Table_list aux = list;
-  while (aux->next != NULL)
-  {
-    if (strcmp(aux->next->table_node->name, table->name) == 0)
-      return;
-    aux = aux->next;
-  }
-  Table_list new_node = (Table_list)malloc(sizeof(_table_list));
-  new_node->next = NULL;
-  new_node->table_node = table;
-  new_node->is_defined = is_defined;
-  new_node->arg_list = NULL;
-  aux->next = new_node;
-  if (DEBUG)
-    printf("Adding a new symbol table: %s\n", new_node->table_node->name);
-}
 
-void add_parameter(Node type_spec, Node id)
-{
-  Table_list aux = list;
-  while (strcmp(aux->table_node->name, current_table->name) != 0)
-  {
-    if (DEBUG)
-      printf("%s---%s\n", aux->table_node->name, current_table->name);
-    aux = aux->next;
-    if (DEBUG)
-      printf("Problem in table node\n");
-    Sym_list a = aux->table_node;
-    if (DEBUG)
-      printf("Problem in table node name\n");
-    char *b = a->name;
-    if (DEBUG)
-      printf("Problem in table\n");
-    b = current_table->name;
-  }
-  Arg_list args = aux->arg_list;
-  if (args != NULL)
-  {
-    while (args->next != NULL)
-      args = args->next;
-  }
-  Arg_list new_arg = (Arg_list)malloc(sizeof(_arg_list));
-  new_arg->label = type_spec->label;
-  if (id != NULL)
-    new_arg->name = id->value;
-  new_arg->next = NULL;
-  if (args != NULL)
-    args->next = new_arg;
-  else
-    aux->arg_list = new_arg;
-}
+
+
 
 char *get_label_string(Label label)
 {
@@ -624,7 +387,7 @@ void full_expand(Node node)
 
 char *get_string_for_tables(Label label)
 {
-  char *s = NULL;
+  char *s;
   switch (label)
   {
   case Char:
@@ -642,10 +405,11 @@ char *get_string_for_tables(Label label)
   case Void:
     s = "void";
   case Program:
-    s = "void";
+    s = "void"; //hol up this is weird
     break;
   default:
-    printf("never ever pc cucks");
+    s = "\n\n\nTHIS SHOULD NEVER HAPPEN!\n\n\n";
+    printf("%s", s);
     break;
   }
   return s;
@@ -653,7 +417,7 @@ char *get_string_for_tables(Label label)
 
 Arg_list get_function_args(char *name)
 {
-  Table_list aux = list;
+  Table_list aux = global_table;
   while (aux != NULL)
   {
     if (strcmp(aux->table_node->name, name) == 0)
@@ -665,21 +429,78 @@ Arg_list get_function_args(char *name)
   return NULL;
 }
 
-Sym_list find_function_entry(Table_list list, Label label, char *value)
+Table_list find_function_entry(char* name)
 {
-  Table_list aux = list;
-  while (aux != NULL)
-  {
-    if (strcmp(aux->table_node->name, value) == 0)
-    {
-      aux->is_defined = 1;
-      return aux->table_node;
-    }
+  Table_list aux = global_table;
+  while (aux != NULL) {
+    if (strcmp(aux->table_node->name, name) == 0)
+      return aux;
     aux = aux->next;
   }
+  return NULL;
+}
+
+Table_list create_function_entry(char* name, Label label, Node paramList)
+{
+  // Assumes it's not already on the table
+  Table_list aux = global_table;
+  while (aux->next != NULL)
+    aux = aux->next;
+
+  Table_list new_node = (Table_list)malloc(sizeof(_table_list));
+  new_node->next = NULL;
+  new_node->is_defined = 0; // assumes not defined, easy to change on what is returned
+  
+
   Sym_list new_list = (Sym_list)malloc(sizeof(_Sym_list));
   new_list->label = label;
-  new_list->name = value;
-  add_table(new_list, 1);
-  return new_list;
+  new_list->name = name;
+  new_node->table_node = new_list;
+
+  // A bit messy, but it adds all the arguments at once, if there are any
+  // Might refactor later, but I think it works anyway
+  Node paramDec = paramList->child;
+  Node type_spec = paramDec->child;
+  Node id = type_spec->brother;
+
+  Arg_list args = (Arg_list)malloc(sizeof(_arg_list));
+  args->label = type_spec->label;
+
+  if (id != NULL)
+    args->name = id->value;
+  else
+    args->name = NULL;
+
+  args->next = NULL;
+
+  new_node->arg_list = args;
+
+  paramDec = paramDec->brother;
+
+  while(paramDec!=NULL){
+    type_spec = paramDec->child;
+    id = type_spec->brother;
+
+    Arg_list new_arg = (Arg_list)malloc(sizeof(_arg_list));
+    new_arg->label = type_spec->label;
+
+    if (id != NULL)
+      new_arg->name = id->value;
+    else
+      new_arg->name = NULL;
+
+    new_arg->next = NULL;
+    args->next = new_arg;
+    args = new_arg;
+
+    paramDec = paramDec->brother;
+  }         
+
+  if (DEBUG)
+    printf("Adding a new symbol table: %s\n", new_node->table_node->name);
+  
+  aux->next = new_node;
+  insert_symbol(global_table->table_node, name, label);
+
+  return new_node;
 }
--- semantics.h
+++ semantics.h
@@ -3,16 +3,14 @@
 char* get_string_for_tables(Label label);
 int handle_node(Node node); 
 void semantic_analysis(Node node);
-void add_table(Sym_list table, int is_defined); 
 void print_tables();
 char* get_label_string(Label label);
-void add_parameter(Node type_spec, Node id);
+//void add_parameter(Node type_spec, Node id);
 void full_expand(Node node);
 Arg_list get_function_args(char * name);
-Sym_list find_function_entry(Table_list list, Label label, char *value);
+Table_list find_function_entry(char* name);
+Table_list create_function_entry(char* name, Label label, Node paramList);
 
 
-Table_list list;
-Sym_list current_table;
-Sym_list global_table;
-
+Table_list global_table;
+Table_list current_table;
