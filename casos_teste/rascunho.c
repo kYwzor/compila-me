c2707020c42c663f32a1966e155bfb11e8d53792
soren5 <pedrofilipecarvalho@mac.com>
2018-05-01 18:47:20 +0100

    output mais limpo

--- casos_teste/rascunho.c
+++ casos_teste/rascunho.c
@@ -1,29 +1,5 @@
-int meta3(int xd, char lmao, double ups, short huehue) {
-    
-    double v = 2.4;
-    xd == huehue;
-    xd > huehue;
-    xd < huehue;
-    xd >= huehue;
-    xd <= huehue;
-    xd | huehue;
-    xd & huehue;
-    xd != huehue;
-    xd && huehue;
-    xd || huehue; 
-    xd ^ huehue;
-    xd = xd % huehue;
-    xd = xd * huehue;
-    xd = xd / huehue;
-    !xd;
-    +xd;
-    -xd;
-    +xd = huehue;
-}
-int f2(char, char);
-int f6(char, int);
+int b(char);
 
-int f3 (char x);
-
-int f3 (char x) {
+int b(char c) {
+  int x = 'a';
 }
