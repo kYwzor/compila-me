fa66f74afb728c8ddf0a2c8588434f1db914f7fb
soren5 <pedrofilipecarvalho@mac.com>
2018-05-01 17:42:41 +0100

    Outputs atualizados para novo caso de teste

---
+++ casos_teste/error_less_xico.c
@@ -0,0 +1,237 @@
+int x(void, char) {
+    int a, b, c; d = 2, a = 'a';
+    int a = b2 != x;
+}
+
+double lmao(char in,int,void,double, int);
+
+int main(char x, int y, int z) {
+    int x = 1, y = 2, z;
+    int a,b,c,d,e;
+    if (x > y && z > y || z != x) {
+        while (1){
+            return 1;
+        }
+    }
+    else {
+        return 0;
+    }
+}
+
+int expressions(void) {
+    int i = 2,a = 3 ,b = 4,c,d;
+    int variable = 1;
+    double x;
+    variable = +variable;
+    char x,y;
+    int a,b;
+    x ^ y;
+    variable = 2,3 & a = 2;
+    variable = variable + 2;
+    variable = variable - 2;
+    (variable) == 2;
+    (variable) > 2;
+    (variable) < 2;
+    (variable) >= 2;
+    (variable) <= 2;
+    (variable) | 2;
+    (variable) & 2;
+    (variable) != 2;
+    (variable) && 2;
+    (variable) || 2; 
+    variable ^ 2;
+    variable = variable % 2;
+    variable = variable * 2;
+    variable = variable / 2;
+    !variable;
+    +variable;
+    -variable;
+    +variable = 2;
+    i = (!i ^ 1)  + (a + b | c % d);
+    i = (a ^ 1) + !(a * b || a % c) && (a);
+    (a  = b);
+    (a = b + 1, c == d/+-!x%4, x || a,y & b, z ^ d);
+
+    putchar(x,variable);
+    a(1,2,getchar(),a(1,2));
+    (1,2,a,b,x,y);
+    putchar();
+}
+
+int statements(void) {
+    
+    int x = 1;
+
+    while(1,2) {
+        
+        if (x) {
+            if (x) {
+                putchar(x);
+                return 1;
+            }
+            else if (1) {
+                putchar(x);
+            }
+            else {
+                putchar(x); 
+            }
+        }
+        else {
+            while(1) {
+                return 0;
+            }
+        }
+    }
+    if (x == 1 && x != 1){
+        return 1;
+    }
+    if (!x) {
+        putchar('o');
+    }
+    else if (oi,wk){
+        gets();
+    }
+
+    if (a) {
+        i;
+    }
+}
+
+
+int oi(int tudo,int bem){
+    char a;
+    char b, c = 'd';
+    double d,e,f;
+    e = 4;
+    f = d+e;
+}
+
+int huehue(int x){
+   char a = x, r = a -b; 
+   putchar(x=a,a+v,c-d,!b,+a,-b);
+}
+
+int meta3(int xd, char lmao, double ups, short huehue) {
+    
+    double v = 2.4;
+    xd == huehue;
+    xd > huehue;
+    xd < huehue;
+    xd >= huehue;
+    xd <= huehue;
+    xd | huehue;
+    xd & huehue;
+    xd != huehue;
+    xd && huehue;
+    xd || huehue; 
+    xd ^ huehue;
+    xd = xd % huehue;
+    xd = xd * huehue;
+    xd = xd / huehue;
+    !xd;
+    +xd;
+    -xd;
+    +xd = huehue;
+}
+
+int f2(char, char);
+int f6(char, int);
+
+int f3 (char x);
+
+int f3 (char x) {
+}
+
+int f4 (char x) {
+
+}
+
+int f(char x);
+
+int f1(char x, int y) {
+  char x = 2.3;
+}
+
+
+int b(char);
+
+int b(char c) {
+  int x = 'a';
+}
+
+int f5(char a, int b);
+
+int f5(int a, int b) {
+  void b;
+  int n;
+  int n;
+
+  int b;
+  int c, b;
+}
+
+int f7(char a, int b) {
+}
+
+
+int k;
+
+int v;
+
+int f8(char y) {
+  char x = y;
+}
+
+int f9(int x, double x);
+
+
+int teste(void) {
+  int b = b;
+}
+
+void main1(void) {
+  int c;
+  int c;
+  int m;
+
+ }
+
+void rekt(void) {
+
+}
+
+void teste;
+
+int help(int x, int y);
+
+void help1(int x, char y){
+    void upsu;
+    char h;
+}
+
+int help2(int x, int y){
+    int wgdsg;
+    char gsfrgs;
+    void teste;
+
+}
+
+void f10(void);
+
+void f10(void) {
+
+}
+
+void f(void a, void) {
+  void b = 2.3;
+}
+
+int c;
+void d;
+int e;
+
+int g (void) {
+}
+
+/* void b; */
+/* int b = asd(b); */
