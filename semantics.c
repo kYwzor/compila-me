ef052da1768f9fa0d342215c56bc39b84d3a829f
kYwzor <tiago_gomes_76@hotmail.com>
2018-05-03 15:10:26 +0100

    minor fixes and comments

--- semantics.c
+++ semantics.c
@@ -209,8 +209,9 @@ int handle_node(Node node)
       printf("%s is %s\n", get_label_string(node->label), get_label_string(Declaration));
     Node type_spec = node->child;
     Node id = type_spec->brother;
-    Node aux = id->brother;
     insert_symbol(current_table, id->value, type_spec->label);
+
+    Node aux = id->brother;
     while(aux != NULL){
       put_type(aux);
       aux = aux->brother;
@@ -270,12 +271,13 @@ int handle_node(Node node)
     handle_node(node->child);
     put_type(node->child);
     put_type(node->child->brother);
+    /*
+    Segundo o big nuno isto nao e assim
     if(node->child->type == undef){
       node->type = undef; 
     }
-    else{
-      node->type = node->child->brother->type; 
-    }
+    */
+    node->type = node->child->brother->type; 
     if (node->brother != NULL)
       handle_node(node->brother);
     if (DEBUG)
@@ -569,7 +571,7 @@ char *get_string_for_tables(Label label)
     break;
   default:
     s = get_label_string(label);
-    //printf("\n\n\nTHIS SHOULD NEVER HAPPEN!\n\n\n");
+    printf("\n\n\nTHIS SHOULD NEVER HAPPEN!\n\n\n");
     break;
   }
   return s;
@@ -733,11 +735,11 @@ void put_type(Node node)
           if (DEBUG)
             printf("%s encontrado na tabela global\n", node->value);
           node->type = symbol_entry->label;
+
           Table_list aux = find_function_entry(node->value);
+          //se for uma funcao
           if (aux != NULL)
-          {
             node->arg_list = aux->arg_list;
-          }
         }
       }
       if(symbol_entry == NULL){
@@ -746,24 +748,25 @@ void put_type(Node node)
     }
     break;
   }
-    case Void:
-    case Char:
-    case Double:
-    case Int:
-    case Short:
-      node->type = node->label;
-      break;
-    default:
+  case Void:
+  case Char:
+  case Double:
+  case Int:
+  case Short:
+    node->type = node->label;
+    break;
+
+  default:
+  {
+    if (node->type == Empty)
     {
-      if (node->type == Empty)
-      {
-        /*THIS IS A HACK, O HOMEM DOS KEBABS SABERA COMO FAZER ISTO DECENTEMENTE
+      /*THIS IS A HACK, O HOMEM DOS KEBABS SABERA COMO FAZER ISTO DECENTEMENTE
       printf("Putting type for %s\n", get_label_string(node->label));
       printf("---\nThis should never happen\n---\n");
       SE ELE NAO SOUBER VAMOS TENTAR FAZER ISTO EM HORAS MAIS NORMAIS*/
-        node->type = Empty;
-      }
-      break;
+      node->type = Empty;
     }
-    }
+  break;
+  }
+  }
 }
