0f7009f7973d90cfc519778d6adbd2393fdeb792
soren5 <pedrofilipecarvalho@mac.com>
2018-06-01 01:39:44 +0100

    expression tests

--- llvm_testes/expressions.c
+++ llvm_testes/expressions.c
@@ -1,8 +1,17 @@
-int main(void){
-    int a = 1;
-    int b = 2;
-    int c = a + b;
-    int d = 3 + 4;
-    putchar('a');
-    return c;
+void sin(int a, short b, char c){
+    int d = +(a - b * c * c) / a / b / c * a % b;
+    int e = 4;
+    int f = d > e < a <= c >= a;
+    putchar('a' + d);
+    putchar('a' + e);
+    putchar('a' + f);
 }
+
+int main(void){
+    char e = 'a';
+    int a = +(-(-(1 + (1 + 1) * 2 % 4 / 3)));
+    putchar(e + a);
+    sin(1, 2, 3);
+
+}
+
