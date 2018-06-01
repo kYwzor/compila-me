e95e1ae7ff89dfc8609a93d27202c6542847f86f
kYwzor <tiago_gomes_76@hotmail.com>
2018-06-01 17:04:14 +0100

    global testing

---
+++ llvm_testes/globalfuckery.c
@@ -0,0 +1,28 @@
+int a = 1;
+int b; 
+int c = 3;
+
+int main(void){
+	int b = -4;
+
+	putchar(c + 'A');
+	putchar('\n');
+	hm(b, a);
+	int a = -2;
+	return;
+}
+
+int d = 4;
+int c = 5;
+
+void hm(int a, int b){
+	putchar(a + 'A');
+	putchar('\n');
+	putchar(b + 'A');
+	putchar('\n');
+	putchar(c + 'A');
+	putchar('\n');
+	int d = -2;
+	putchar(d + 'A');
+	putchar('\n');
+}
