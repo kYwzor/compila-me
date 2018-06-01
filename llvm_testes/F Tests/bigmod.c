d5236d5d05345d4ff4784e8cd2e29cb933007d1a
kYwzor <tiago_gomes_76@hotmail.com>
2018-06-01 15:57:05 +0100

    tests for F

---
+++ llvm_testes/F Tests/bigmod.c
@@ -0,0 +1,25 @@
+int bigmod(int a ,int p,int m);
+
+int main()
+{
+      int a;
+      int p,m;
+
+      putchar(bigmod(5,55,231)));
+
+      return 0;
+}
+
+int bigmod(int a ,int p,int m)
+{
+            if(p==0)
+            return 1;
+
+            if(p%2==0){                  // p is even then split it up and mod
+            int c=bigmod(a,p/2,m);
+            return ( (c%m) * (c%m) )%m;
+            }
+            else  // p is odd then make it even
+            return ( (a%m)* bigmod(a,p-1,m) ) %m;
+
+}
